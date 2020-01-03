f8a5de850b5b71833fd8f152953a071f51b8e4c6
Etienne BLANC <etienne.blanc94@gmail.com>
2020-01-03 19:09:45 +0100

    add pytest.ini to unshow a warning and update 'args'

--- katas/args/args.py
+++ katas/args/args.py
@@ -1,9 +1,26 @@
 
 
+def getArgsDictFromList(argsList):
+    "Return a Dict of [arg: [parameters]] or [arg: True]"
+
+    argsDict = {}
+
+    for arg in argsList:
+        argSplit = arg.split()
+
+        if len(argSplit) == 1:
+            argsDict[argSplit[0]] = True
+        else:
+            argsDict[argSplit[0]] = argSplit[1]
+    print(argsDict)
+    return argsDict
+
+
 def getArgsDict(argsString):
     "Return a Dict of key/values of gived args (as a string)"
-    argsString.split("-")
-    return {1: 1, 2: 2, 3: 3}
+    args = argsString[1:].split(' -')
+
+    return args
 
 
 if __name__ == "__main__":
--- katas/args/test_args.py
+++ katas/args/test_args.py
@@ -1,5 +1,17 @@
-from args import getArgsDict
+from args import getArgsDict, getArgsDictFromList
 
 
 def test_getArgsDict():
-    assert len(getArgsDict("-l -p 8080 -d /usr/logs")) == 3
+    args = "-l -p 8080 -d /usr/logs"
+
+    assert len(getArgsDict(args)) is 3
+    #assert getArgsDict(args)[0] == ["l"]
+    #assert len(getArgsDict(args)[1]) == 2
+
+
+def test_getArgsDictFromList():
+    argsList = ["l", "p 8080", "d /usr/logs"]
+
+    assert len(getArgsDictFromList(argsList)) is 3
+    assert getArgsDictFromList(argsList) is {
+        'd': '/usr/logs', 'l': True, 'p': '8080'}
