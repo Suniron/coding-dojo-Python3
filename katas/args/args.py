aed756697b581ad6708acbdd51f6f5ed6b2ceccc
Etienne BLANC <etienne.blanc94@gmail.com>
2020-01-04 20:08:29 +0100

    Continue Args

--- katas/args/args.py
+++ katas/args/args.py
@@ -1,27 +1,85 @@
+import sys
+
+parameters = {
+    "l": {"description": "Logging", "type": "boolean", "default_value": True},
+    "p": {"description": "Port", "type": "integer", "default_value": 0},
+    "d": {"description": "Directory", "type": "string", "default_value": ""}
+}
+
+
+def getDefaultValue(flag):
+    "Return defaut value for flag"
+
+    # Remove - if here
+    if flag[0] == '-':
+        flag = flag[1:]
+
+    if flag in parameters.keys():
+        return parameters[flag]['default_value']
+
+    else:
+        # TODO: raise exeption ?
+        print("Error:", flag, "is unknow flag.")
+        return "error"
 
 
 def getArgsDictFromList(argsList):
-    "Return a Dict of [arg: [parameters]] or [arg: True]"
+    "Return a Dict of [arg: [parameters]] or [arg: True]. TODO: Check if flag exist in parameters"
 
     argsDict = {}
 
-    for arg in argsList:
-        argSplit = arg.split()
+    flag = None
 
-        if len(argSplit) == 1:
-            argsDict[argSplit[0]] = True
+    for arg in argsList:
+        # If it's flag or negative number:
+        if arg[0] is '-':
+            arg = arg[1:]
+            # Check if number and if flag is already set
+            if arg.isdigit() and flag:
+                argsDict[flag] = arg
+                flag = None  # Reinit flag
+
+            else:
+                if flag is None:
+                    if parameters[arg]["type"] == "boolean":
+                        argsDict[arg] = True
+                    else:
+                        flag = arg
+                        if len(argsList) == 1:
+                            argsDict[flag] = getDefaultValue(flag)
+
+                else:
+                    # Get and put default value last flag
+                    argsDict[flag] = getDefaultValue(flag)
+                    # Set next flag:
+                    flag = arg
+
         else:
-            argsDict[argSplit[0]] = argSplit[1]
-    print(argsDict)
+            if flag is None:  # If no flag for this
+                print("Error: no flag for '"+arg+"'!")
+            else:
+                flagType = parameters[flag]["type"]
+
+                # Check type:
+                if flagType is "integer":
+                    argsDict[flag] = int(arg)
+
+                elif flagType is "string":
+                    argsDict[flag] = arg
+
     return argsDict
 
 
-def getArgsDict(argsString):
-    "Return a Dict of key/values of gived args (as a string)"
-    args = argsString[1:].split(' -')
+def getSchema(args):
+    "Return a description of settings used"
+    # TODO: Continue here
 
-    return args
+    return
 
 
 if __name__ == "__main__":
-    pass
+    # Use case:
+    # call this program with "python args.py -l -p 8080 -d /usr/logs"
+    args = sys.argv[1:]
+
+    print(getArgsDictFromList(args))
--- katas/args/test_args.py
+++ katas/args/test_args.py
@@ -1,17 +1,30 @@
-from args import getArgsDict, getArgsDictFromList
-
-
-def test_getArgsDict():
-    args = "-l -p 8080 -d /usr/logs"
-
-    assert len(getArgsDict(args)) is 3
-    #assert getArgsDict(args)[0] == ["l"]
-    #assert len(getArgsDict(args)[1]) == 2
+from args import getArgsDictFromList, getDefaultValue
 
 
 def test_getArgsDictFromList():
-    argsList = ["l", "p 8080", "d /usr/logs"]
-
+    argsList = ['-l', '-p', '8080', '-d', '/usr/logs']
+    # Check length
     assert len(getArgsDictFromList(argsList)) is 3
-    assert getArgsDictFromList(argsList) is {
-        'd': '/usr/logs', 'l': True, 'p': '8080'}
+    assert len(getArgsDictFromList(
+        ['-l', '-p', '8080', '-d', '/usr/logs/my-logs'])) is 3
+    assert len(getArgsDictFromList(['-l', '-p', '-8080'])) is 2
+    # Check returned type
+    assert type(getArgsDictFromList(argsList)) is dict
+    # Check element types
+    assert type(getArgsDictFromList(argsList)["l"]) is bool
+    assert type(getArgsDictFromList(argsList)["p"]) is int
+    assert type(getArgsDictFromList(argsList)["d"]) is str
+    # Check default values
+    assert getArgsDictFromList(["-p"])["p"] is 0
+    assert getArgsDictFromList(["-d"])["d"] is ""
+
+
+def test_getDefautValue():
+    # Check default value
+    assert getDefaultValue("l") is True
+    assert getDefaultValue("p") is 0
+    assert getDefaultValue("d") is ""
+    # Check with -
+    assert getDefaultValue("-p") is 0
+    # Check error with unknow flag
+    assert getDefaultValue("0") is "error"
